00fbd1ba93d2cd954edc4230947b2bb3cac93a5b
@McDermott <ra_mcd@yahoo.com>
2019-09-10 21:23:07 +0100

    Adding validation.

--- atm-app/src/app/app.component.ts
+++ atm-app/src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+const SECRET_KEY = [1,1,1,1];
 
 @Component({
   selector: 'app-root',
@@ -14,17 +18,25 @@ export class AppComponent {
   public timeInfo: string;
   public isLoginPage: boolean;
   public pin: string [];
-  public secretPin: number;
   public pinInput: number[];
+  public basePinState: number[];
   public enteredItems: number;
+  public canBeAltered: boolean;
+  public error: string;
   
-  constructor() {
+  constructor(private http: HttpClient) {
+
+    // this.error = "ERROR";
 
     this.enteredItems = 0;
 
-    this.pinInput = [-1, -1, -1, -1];
+    this.basePinState = [-1,-1,-1,-1];
 
-    this.secretPin = 1111;
+    this.pinInput = [-1,-1,-1,-1];
+
+    this.canBeAltered = true;
+
+
 
     this.pin = [' _ ',' _ ',' _ ',' _ '];
 
@@ -57,19 +69,97 @@ export class AppComponent {
 
   }
 
+  public getData(pin: any): Observable<any> {
+    return this.http.post<any>('https://frontend-challenge.screencloud-michael.now.sh/api/pin/', pin)
+      .pipe();
+  }
+
   public pinPageHandler(input: any): void {
-    if (typeof input === 'number') {
+
+    this.error = "";
+
+    const successfulAccess: any = (incomingPinString: string) => {
+      const payLoad = {
+        pin: incomingPinString
+      }
+      this.getData(payLoad).subscribe((response: any) => {
+        console.log("TCL: AppComponent -> successfulAccess:any -> response", response)
+
+      })
+    }
+
+    const clearNumber: any = (input: any) => {
+      let numberOfNegatives: number = 0;
+      this.pinInput.forEach((pin) => {
+        if (pin === -1) {
+          numberOfNegatives++;
+        }
+      })
+      if (this.canBeAltered) {
+        this.pinInput[((this.pinInput.length - 1) - numberOfNegatives)] = -1;
+        this.pin[((this.pinInput.length - 1) - numberOfNegatives)] = " _ ";
+        this.enteredItems = ((this.pinInput.length - 1) - numberOfNegatives);
+      }            
+      if (this.pinInput.toString() === this.basePinState.toString()) {
+        this.canBeAltered = false;
+      } else {
+        this.canBeAltered = true;
+      }
+    }
+
+
+
+    if ((typeof input === 'number') && this.isLoginPage) {
       if ((this.pinInput[this.enteredItems] === -1) && (this.enteredItems <= 3)) {
         this.pinInput[this.enteredItems] = input;
         this.enteredItems = this.enteredItems + 1;
-        console.log(this.pinInput);
+        this.pin[(this.enteredItems - 1)] = " * ";
       }
     } else {
       switch (input) {
-        case 'CANCEL'
+        case 'CLEAR':
+          clearNumber();
+        break;
+        case 'CANCEL':
+          if (this.isLoginPage) {
+            this.enteredItems = 0;
+            this.pin = [' _ ',' _ ',' _ ',' _ '];
+            this.pinInput = [-1,-1,-1,-1];
+            this.error = "";
+          } else {
+            this.isLoginPage = !this.isLoginPage;
+            this.enteredItems = 0;
+            this.pin = [' _ ',' _ ',' _ ',' _ '];
+            this.pinInput = [-1,-1,-1,-1];
+            this.error = "";
+          }
+        break;
+        case '*':
+          if (this.isLoginPage) {
+            this.error = "OOPS, STAR DOESN'T DO MUCH.";
+          }
+        break;
+        case '#':
+            if (this.isLoginPage) {
+              this.error = "YIPES!, THE POUND IS USELESS.";
+            }
+        break;
+        case 'OK':
+          if (this.isLoginPage) {
+            if (this.pinInput.toString() === SECRET_KEY.toString()) {
+              let concatElements: string = "";
+              this.pinInput.forEach(i => concatElements += i.toString());
+              successfulAccess(concatElements);
+            } else {
+              this.error = "INCORRECT PIN NUMBER!";
+            }
+          }
+        break;
       }
     }
-    console.log('pinPageHandler', input);
+
+
+    // console.log('pinPageHandler', input);
   }
 
   public atmPageHandler(input: any): void {
